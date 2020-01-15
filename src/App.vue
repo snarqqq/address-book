89762d32accca7cbc6c4afb4cad4547c72afa413
snarqqq <yttkym2357@gmail.com>
2020-01-15 15:03:42 +0900

    created Loading.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <v-app>
+    <Loading v-show="loading"></Loading>
     <v-app-bar
       app
       color="primary"
       dark
+      v-show="!loading"
     >
       <v-app-bar-nav-icon v-show="$store.state.login_user" @click.stop="toggleSideMenu"></v-app-bar-nav-icon>
       <v-toolbar-title>Address Book</v-toolbar-title>
@@ -14,20 +16,27 @@
     </v-app-bar>
     <SideNav/>
 
-    <v-content>
+    <v-content v-show="!loading">
       <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Loading from './components/Loading'
 import firebase from 'firebase'
 import {mapActions} from 'vuex'
 import SideNav from './components/SideNav'
 export default {
   name: 'App',
   components: {
+    Loading,
     SideNav
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000)
   },
   created () {
     firebase.auth().onAuthStateChanged(user => {
@@ -43,9 +52,11 @@ export default {
       }
     })
   },
-  data: () => ({
-    //
-  }),
+  data () {
+    return {
+      loading: true
+    }
+  },
   methods: {
     // ES6の分割代入構文
     ...mapActions(['toggleSideMenu', 'setLoginUser', 'logout', 'deleteLoginUser', 'fetchAddresses'])
